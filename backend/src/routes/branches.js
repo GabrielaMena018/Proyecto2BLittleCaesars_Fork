c3e50ebe2d84cd78b650818252103828f86ea210
Gaby's <20240153@ricaldone.edu.sv>
2026-06-23 15:12:24 +0000

    fix3

--- backend/src/routes/branches.js
+++ backend/src/routes/branches.js
@@ -1,5 +1,5 @@
 import express from "express";
-import branchescpntoller from "../controllers/branchesController.js";
+import branchescontoller from "../controllers/branchesController.js";
 import { validateAuthCookie } from "../middlewares/authMiddleware.js";
 
 //Router() nos ayuda a colocar los métodos
@@ -8,14 +8,14 @@ const router = express.Router();
 
 router
   .route("/")
-  .get( branchescpntoller.getbranches)
-  .post(branchescpntoller.insertBranches);
+  .get( branchescontoller.getbranches)
+  .post(branchescontoller.insertBranches);
 
 
 
 router
   .route("/:id")
-  .put(branchesController.updateBranches)
+  .put(branchescontoller.updateBranches)
   .delete(validateAuthCookie(["admin"]),branchesController.deleteBranches);
 
 export default router;
